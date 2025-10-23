069c1e93c849fbe501d7d3af6c911b53f0b2e33b
rylang751 <jahzierejohnsonm@gmail.com>
2025-10-23 13:39:59 -0400

    Pause Menu Done for now

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public static bool gameIsPaused = false; 
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,22 +43,32 @@ public class PauseMenu : MonoBehaviour
         {
             pauseMenu.SetActive(true); // show the pause menu
         }
-        Time.timeScale = 0f; // stop game time
+       Time.timeScale = 0f; // stop game time
         gameIsPaused = true;
         Cursor.visible = true; //show cursor for menu interaction
-        Cursor.lockState = CursorLockMode.None; // Unlock cursor
+       Cursor.lockState = CursorLockMode.None; // Unlock cursor
     }
 
     public void ResumeGame()
     {
-        if (pauseMenu !=null);
+        if (pauseMenu != null);
         {
             pauseMenu.SetActive(false);
         }
         Time.timeScale = 1f;
         gameIsPaused = false;
         Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+       Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitRace()
+    {
+        SceneManager.LoadScene("Character Select");
     }
 
     
