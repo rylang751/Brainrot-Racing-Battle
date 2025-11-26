4b89f243c02442638fffb5d2c11078b41a60f9a9
rylang751 <jahzierejohnsonm@gmail.com>
2025-11-26 13:52:09 -0500

    Pause menu fixed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject StartButton;
+    public GameObject SettingsButton;
+    public GameObject QuitButton;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject StartButton;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Character Select");
     }
 
 }
